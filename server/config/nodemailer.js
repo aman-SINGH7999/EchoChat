d2382cec89971bc91269052ebf9675adef2cd909
Aman Singh <amansing7999@gmail.com>
2026-07-12 03:46:08 +0530

    fix email bug

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,21 +1,19 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
   
-transporter.verify((error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('SMTP OK');
-  }
-});
+// transporter.verify((error) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('SMTP OK');
+//   }
+// });
 
 module.exports = { transporter };
