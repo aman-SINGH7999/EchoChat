6f2811d5550868f9cdfd74129d38cad506e525b5
Aman Singh <amansing7999@gmail.com>
2026-07-12 03:30:20 +0530

    email fix

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
   
